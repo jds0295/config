d81553ad6e06089514e6bb14cb61cba6393c3090
James Shaw <390742@twgroup.co.nz>
2024-04-23 15:28:15 +1200

    add cmp

--- nvim/lua/plugins/codecompletion.lua
+++ nvim/lua/plugins/codecompletion.lua
@@ -1,6 +1,84 @@
 return {
 	{ --CMP
-
+		"hrsh7th/nvim-cmp",
+		config = function()
+			local cmp = require'cmp'
+			-- cmp_endabled = flase
+			cmp.setup({
+				-- Snippet
+				snippet = {
+					expand = function(args)
+						vim.fn["vsnip#anonymous"](args.body)
+					end
+				},
+				-- Sources
+				sources = {
+					{ name = 'nvim_lsp', keyword_length = 3},
+					{ name = 'vsnip', keyword_length = 2},
+					{ name = 'path' },
+					{ name = 'buffer',keyword_length = 2 },
+					{ name = 'cmdline' },
+					{ name = 'nvim_lua', keyword_length = 2},
+					-- { name = 'nvim_lsp_signature_help' },
+				},
+				-- formatting
+				formatting = {
+					fields = {'menu', 'abbr', 'kind'},
+					format = function(entry, item)
+						local menu_icon = {
+							nvim_lsp = 'λ',
+							vsnip = '󰁨',
+							buffer = '',
+							path = '',
+							cmdline = '',
+							nvim_lua = '󰢱',
+						}
+						item.menu = menu_icon[entry.source.name]
+						return item
+					end,
+				},
+				-- Window styling
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
+				-- Mappings 
+				mapping = {
+					['<M-Space>'] = cmp.mapping.complete(), -- to bring up the code completion at current cursor
+					['<Tab>'] = cmp.mapping.select_next_item(), -- Move to next entry in the list
+					['<S-Tab>'] = cmp.mapping.select_prev_item(), -- Move to previous entry in the list
+					['<C-e>'] = cmp.mapping.close(), -- to close the text complete popup
+					['<CR>'] = cmp.mapping.confirm({
+						behavior = cmp.ConfirmBehavior.Insert,
+						select = true,
+					}) -- insert the currently selected suggesion
+				},
+			})
+		end
+	},
+	{
+		"neovim/nvim-lspconfig",
+	},
+	{
+		"hrsh7th/cmp-nvim-lsp"
+	},
+	{
+		"hrsh7th/cmp-nvim-lua"
+	},
+	{
+		"hrsh7th/cmp-buffer"
+	},
+	{
+		"hrsh7th/cmp-path"
+	},
+	{
+		"hrsh7th/cmp-cmdline"
+	},
+	{
+		"hrsh7th/cmp-vsnip"
+	},
+	{
+		"hrsh7th/vim-vsnip"
 	},
 	{ -- copilot
 		"github/copilot.vim",
