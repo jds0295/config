a10fd90728e5cbed8be73cb31216645611d17e8f
James Shaw <390742@twgroup.co.nz>
2024-08-16 14:44:47 +1200

    updates

--- nvim/lua/plugins/whichkey.lua
+++ nvim/lua/plugins/whichkey.lua
@@ -1,12 +1,26 @@
 return {
-		"folke/which-key.nvim",
-		config = function()
-			vim.o.timeout = true
-			vim.o.timeoutlen = 300
-			require("which-key").setup {
-				-- your configuration comes here
-				-- or leave it empty to use the default settings
-				-- refer to the configuration section below
-			}
-		end
-	}
+	"folke/which-key.nvim",
+	config = function()
+		vim.o.timeout = true
+		vim.o.timeoutlen = 300
+
+		local wk = require("which-key")
+
+		-- Register custom key mappings
+		wk.register({
+			["<C-a>"] = { "Increment number under cursor" },
+			["<C-x>"] = { "Decrement number under cursor" },
+			["g<C-a>"] = { "Increment number in visual block mode" },
+			["g<C-x>"] = { "Decrement number in visual block mode" },
+		}, { mode = "n" })  -- Normal mode mappings
+
+		wk.register({
+			["g<C-a>"] = { "Increment number in visual block mode" },
+			["g<C-x>"] = { "Decrement number in visual block mode" },
+		}, { mode = "v" })  -- Visual mode mappings
+
+		-- Setup which-key
+		wk.setup {
+		}
+	end
+}
