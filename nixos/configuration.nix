b235be2b6a65cbc94bbc4280fc2b8f62569cc6b9
James Shaw <jamesshawnz@gmail.com>
2025-06-09 11:54:20 +1200

    displaylink

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -9,6 +9,7 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ./keyremap.nix
+			./displaylink.nix
     ];
 
   # Bootloader.
@@ -121,7 +122,9 @@
     git
     neovim
     starship
+		thefuck
     tmux
+		usbutils
     vim
   ];
 
