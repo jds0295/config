b1ecd7a39f1e366b1390d665abdbf08c6e19c1dc
James Shaw <jamesshawnz@gmail.com>
2025-12-01 14:11:25 +1300

    dual monitor and single wp picker

--- scripts/pick-wallpaper.sh
+++ scripts/pick-wallpaper.sh
@@ -5,11 +5,12 @@ WALLPAPER_DIR="/mnt/red/rescue from ubuntu/Wallpaper-Bank/wallpapers"
 CHOICE=$(find "$WALLPAPER_DIR" -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.jpeg" \) | sort | (pidof wofi ||wofi --dmenu --insensitive --prompt "Pick a wallpaper:"))
 
 if [[ -n "$CHOICE" ]]; then
-  swww img "$CHOICE" --transition-type any
+  swww img "$CHOICE" --transition-type random
+  # swww img "$CHOICE" --outputs "$ACTIVE_MONITOR"
   wal -i "$CHOICE" -n --cols16
   tmux source-file ~/.config/tmux/tmux.conf
   cat ~/.config/starship.toml ~/.cache/wal/colors-starship.toml > ~/.config/starship-colored.toml
-  export STARSHIP_CONFIG = ~/.config/starship-colored.toml
+  export STARSHIP_CONFIG=~/.config/starship-colored.toml
   # starship
   notify-send "Wallpaper set" "$(basename "$CHOICE")" -i "$CHOICE"
 fi
